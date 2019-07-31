64766737a9640122003335154cff5a2c1ccd3298
Mikael Peltomaa <mikael.peltomaa@gmail.com>
2019-07-31 08:35:11 +0000

    initial

---
+++ main.js
@@ -0,0 +1,51 @@
+let mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxkZXJ4IiwiYSI6ImNqdHNrdHlmbDA1bjczem81ZTQzZnJ3engifQ.2PoeE03vtRBPj1D_-ESbrw';
+
+let mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+
+let thunderforest_apikey = 'f9db91884c8c434b8a6d6d25789af2e5';
+
+// Mapbox maps
+let mapbox_light = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr});
+let mapbox_dark = L.tileLayer(mbUrl, {id: 'mapbox.dark', attribution: mbAttr});
+let mapbox_streets = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr});
+let mapbox_satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite',   attribution: mbAttr});
+let mapbox_satellite_streets = L.tileLayer(mbUrl, {id: 'mapbox.streets-satellite',   attribution: mbAttr});
+let mapbox_outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors',   attribution: mbAttr});
+let mapbox_run_bike_hike = L.tileLayer(mbUrl, {id: 'mapbox.run-bike-hike',   attribution: mbAttr});
+
+// Thunderforest maps
+let thunderforest_outdoors = L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: thunderforest_apikey,
+	maxZoom: 22
+});
+
+let thunderforest_landscape = L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: thunderforest_apikey,
+	maxZoom: 22
+});
+
+let thunderforest_transport = L.tileLayer('https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: thunderforest_apikey,
+	maxZoom: 22
+});
+
+// OSM maps
+let osm_standard = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var mbt = L.tileLayer('http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &amp; USGS'
+});
+
+
+
+let map = L.map('map', {
+		center: [63.568, 27.181],
+		zoom: 13,
+		layers: [mapbox_streets],
+		drawControl: true
+	});
---
+++ plugins.js
@@ -0,0 +1,84 @@
+// Coordinate
+var c = new L.Control.Coordinates();
+
+c.addTo(map);
+
+map.on('click', function(e) {
+	c.setCoordinates(e);
+});
+
+// DRAW
+var editableLayers = new L.FeatureGroup();
+    map.addLayer(editableLayers);
+
+    var options = {
+        edit: {
+            featureGroup: editableLayers, //REQUIRED!!
+            remove: false
+        }
+    };
+
+    //var drawControl = new L.Control.Draw(options);
+    //map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (e) {
+        var type = e.layerType,
+            layer = e.layer;
+
+        if (type === 'marker') {
+            layer.bindPopup('A popup!');
+        }
+
+        editableLayers.addLayer(layer);
+    });
+
+    document.getElementById('export').onclick = function(e) {
+                // Extract GeoJson from featureGroup
+                var data = editableLayers.toGeoJSON();
+
+                // Stringify the GeoJson
+                var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+
+                // Create export
+                document.getElementById('export').setAttribute('href', 'data:' + convertedData);
+                document.getElementById('export').setAttribute('download','data.geojson');
+            }
+
+
+            // Layer Control
+            let baseLayers = {
+              	"Mapbox light": mapbox_light,
+								"Mapbox dark": mapbox_dark,
+              	"Mapbox streets": mapbox_streets,
+								"Mapbox satellite": mapbox_satellite,
+								"Mapbox satellite streets": mapbox_satellite_streets,
+								"Mapbox outdoors": mapbox_outdoors,
+								"Mapbox run bike hike": mapbox_run_bike_hike,
+								"OSM standard": osm_standard,
+								"Thunderforest outdoors": thunderforest_outdoors,
+								"Thunderforest landscape": thunderforest_landscape,
+								"Thunderforest transport": thunderforest_transport,
+								"MBT": mbt
+            };
+
+            let overlays = {
+              "Editable": editableLayers
+            };
+
+            var allMapLayers = { 'mapbox_grayscale': mapbox_light,
+            					  'mapbox_streets': mapbox_streets,
+												'mapbox_dark': mapbox_dark,
+												'mapbox_satellite': mapbox_satellite,
+												'mapbox_satellite-streets': mapbox_satellite_streets,
+												'mapbox_outdoors': mapbox_outdoors,
+												'mapbox_run-bike-hike': mapbox_run_bike_hike,
+												'osm_standard': osm_standard,
+												'thunderforest_outdoors': thunderforest_outdoors,
+												'thunderforest_landscape': thunderforest_landscape,
+												'thunderforest_transport': thunderforest_transport,
+												'mbt': mbt,
+                        'editable': editableLayers
+            					};
+            L.hash(map, allMapLayers);
+            L.control.layers(baseLayers, overlays).addTo(map);
+            // Layer Control
